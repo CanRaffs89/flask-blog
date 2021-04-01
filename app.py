b878a5b161868952b8c403ce82929263fba192a9
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-04-01 21:39:28 +0100

    Set up Flask forms

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '40014fa522110b5a21b52cca'
+
+class NewPostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    date = StringField("Today's Date", validators=[DataRequired()])
+    image = StringField("Image URL", validators=[DataRequired()])
+    image_text = StringField("Image Description", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 posts = [
     {
         "id": 1,
         "title": "10 Things To Do In Toronto",
         "author": "Jane Doe",
-        "intro_paragraph": "Laborum ullam quo quo quia nesciunt ut fuga illum. Ullam nihil nulla id consequatur minus autem beatae expedita. Rerum sed error id quas at quos est.",
+        "subtitle": "Laborum ullam quo quo quia nesciunt ut fuga illum. Ullam nihil nulla id consequatur minus autem beatae expedita. Rerum sed error id quas at quos est.",
         "content": "\nQui quos quasi ad consectetur facere.\nAd veniam aut quisquam. Molestias et deserunt ex inventore molestiae ab delectus. Hic accusamus voluptatem unde soluta tenetur non quaerat. Labore officiis minima explicabo consectetur quo repellendus voluptates fuga. Dicta excepturi totam optio ea vero eligendi molestias. Unde ut dolorum praesentium similique consequatur tenetur dolorem amet.\nQui eum vitae assumenda consequatur aut nesciunt. Hic beatae et totam. Et quibusdam qui consequatur sint aut accusantium quis.\nQui sed aut assumenda et dolore nihil quia. Nemo ipsam rerum ad id autem voluptatem quidem in.",
         "date_created": "Jan 1, 2021",
         "image": "../static/img/toronto.jpg",
@@ -17,7 +33,7 @@ posts = [
         "id": 2,
         "title": "5 Winter Kayaking Hotspots",
         "author": "John Smith",
-        "intro_paragraph": "Eveniet placeat animi quo tenetur possimus. Aut ea dignissimos quas sed sint saepe tempora et. Molestiae repellat quis nihil accusantium.",
+        "subtitle": "Eveniet placeat animi quo tenetur possimus. Aut ea dignissimos quas sed sint saepe tempora et. Molestiae repellat quis nihil accusantium.",
         "content": "\nQui quos quasi ad consectetur facere.\nAd veniam aut quisquam. Molestias et deserunt ex inventore molestiae ab delectus. Hic accusamus voluptatem unde soluta tenetur non quaerat. Labore officiis minima explicabo consectetur quo repellendus voluptates fuga. Dicta excepturi totam optio ea vero eligendi molestias. Unde ut dolorum praesentium similique consequatur tenetur dolorem amet.\nQui eum vitae assumenda consequatur aut nesciunt. Hic beatae et totam. Et quibusdam qui consequatur sint aut accusantium quis.\nQui sed aut assumenda et dolore nihil quia. Nemo ipsam rerum ad id autem voluptatem quidem in.",
         "date_created": "Nov 6, 2020",
         "image": "../static/img/lake.jpg",
@@ -27,7 +43,7 @@ posts = [
         "id": 3,
         "title": "Montreal's Best Hidden Bars",
         "author": "John Smith",
-        "intro_paragraph": "Ad veniam aut quisquam. Molestias et deserunt ex inventore molestiae ab delectus. Hic accusamus voluptatem unde soluta tenetur non quaerat. Labore officiis minima explicabo consectetur quo repellendus voluptates fuga.",
+        "subtitle": "Ad veniam aut quisquam. Molestias et deserunt ex inventore molestiae ab delectus. Hic accusamus voluptatem unde soluta tenetur non quaerat. Labore officiis minima explicabo consectetur quo repellendus voluptates fuga.",
         "content": "\nQui quos quasi ad consectetur facere.\nAd veniam aut quisquam. Molestias et deserunt ex inventore molestiae ab delectus. Hic accusamus voluptatem unde soluta tenetur non quaerat. Labore officiis minima explicabo consectetur quo repellendus voluptates fuga. Dicta excepturi totam optio ea vero eligendi molestias. Unde ut dolorum praesentium similique consequatur tenetur dolorem amet.\nQui eum vitae assumenda consequatur aut nesciunt. Hic beatae et totam. Et quibusdam qui consequatur sint aut accusantium quis.\nQui sed aut assumenda et dolore nihil quia. Nemo ipsam rerum ad id autem voluptatem quidem in.",
         "date_created": "Dec 15, 2020",
         "image": "../static/img/montreal.jpg",
@@ -46,21 +62,22 @@ def get_post(post_id):
 
 @app.route("/add", methods=["GET", "POST"])
 def add_post():
-    if request.method == "POST":
+    form = NewPostForm()
+    if form.validate_on_submit():
         new_post = {
             "id": (len(posts) + 1),
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "intro_paragraph": request.form["intro"],
-            "content": request.form["content"],
-            "date_created": request.form["date"],
-            "image": request.form["image"],
-            "image_alt_text": request.form["alt_text"]
+            "title": form.title.data,
+            "author": form.author.data,
+            "subtitle": form.subtitle.data,
+            "content": form.content.data,
+            "date_created": form.date.data,
+            "image": form.image.data,
+            "image_alt_text": form.image_text.data
         }
 
         posts.append(new_post)
         return redirect(url_for("home"))
-    return render_template("add.html")
+    return render_template("add.html", form=form)
 
 @app.route("/login")
 def login():
@@ -77,10 +94,10 @@ if __name__ == "__main__":
 # Added temporary dictionary of posts for testing purposes
 # Index page and page for each post working
 
-# DONE TODAY
+# DONE THURS 1
 # Added a basic New Post form for testing
+# Set up Flask WTForms
 
 # DO TODAY
-# Install Flask WTForms (refer to Day 62)
 # Start working on login functionality
 
