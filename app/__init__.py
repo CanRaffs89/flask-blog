8d2f6b97ad32c1672254cdc4f90bfef77f83477b
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-04-25 21:11:23 +0100

    Added final files for production

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,12 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgrespass@localhost/blog'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cipefqatmcxywj:081a7fdfa6fdebff523a8570d788afb0f2f31b4438cd1b077f655a42cf3169f2@ec2-54-205-61-191.compute-1.amazonaws.com:5432/ddedfg2rqcr5pu'
 
 app.config['SECRET_KEY'] = '40014fa522110b5a21b52cca'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
