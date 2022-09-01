a5c46ea33570fe55b179fc80ec2e6f338bf9cff2
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-09-01 20:41:30 +0100

    Changed to new database

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 ENV = 'prod'
 
 if ENV == 'dev':
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgrespass@localhost/blog'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password123@localhost/flask_blog'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://flask_blog_db_user:lVhjVsq1apYSEKgqC2NSs6bVXlNNkqNo@dpg-cc7rbfda4998eoruvfmg-a.frankfurt-postgres.render.com/flask_blog_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask_blog_database_user:qefQtKB8TN9RfNvF6vtg7THl3rdpRbTD@dpg-cc8gfb1a6gdlv717gkc0-a.frankfurt-postgres.render.com/flask_blog_database'
 
 app.config['SECRET_KEY'] = '40014fa522110b5a21b52cca'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
